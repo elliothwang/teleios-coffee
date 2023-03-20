29672eccc1968a86683d8373adfe2c6c0b3fe6d9
elliothwang <elliothwang@gmail.com>
2023-03-20 11:19:39 -0700

    complete web view carousel

--- src/components/CategoryCarousel/CategoryCarousel.jsx
+++ src/components/CategoryCarousel/CategoryCarousel.jsx
@@ -5,11 +5,15 @@ import categories from '../../assets/categories';
 import './CategoryCarousel.styles.scss';
 
 function CategoryCarousel({}) {
+  // const [windowSize, setWindowSize] = useState(getWindowSize());
   const [currentIndex, setCurrentIndex] = useState(0);
-  const length = categories.length;
+  const length = categories && categories.length;
+  const show = length - 1;
+
+  // const getWindowSize()
 
   const next = () => {
-    if (currentIndex < length - 1) {
+    if (currentIndex < length - show) {
       setCurrentIndex((prevState) => prevState + 1);
     }
   };
@@ -22,28 +26,32 @@ function CategoryCarousel({}) {
 
   return (
     <div className="categories-container">
-      {/* {categories.map((category) => (
-        <CategoryCarouselCard key={category.id} category={category} />
-      ))} */}
-
       <div className="carousel-container">
         <div className="carousel-wrapper">
-          <button className="left-arrow" onClick={prev}>
-            &lt;
-          </button>
+          {currentIndex > 0 && (
+            <button className="left-arrow" onClick={prev}>
+              &lt;
+            </button>
+          )}
           <div className="carousel-content-wrapper">
             <div
-              className="carousel-content"
-              style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+              // className="carousel-content"
+              className={`carousel-content show-${show}`}
+              // style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+              style={{
+                transform: `translateX(-${currentIndex * (100 / show)}%)`,
+              }}
             >
               {categories.map((category) => (
                 <CategoryCarouselCard key={category.id} category={category} />
               ))}
             </div>
           </div>
-          <button className="right-arrow" onClick={next}>
-            &gt;
-          </button>
+          {currentIndex < length - show && (
+            <button className="right-arrow" onClick={next}>
+              &gt;
+            </button>
+          )}
         </div>
       </div>
     </div>
