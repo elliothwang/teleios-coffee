2331fc39811bd61ddb6c6739f686e8a2106decce
elliothwang <elliothwang@gmail.com>
2023-05-19 11:35:56 -0700

    shrink navbutton

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/main.scss';
+
+function About() {
+  return <div>About</div>;
+}
+
+export default About;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,11 +1,15 @@
 import Hero from '../components/Hero';
 import Carousel from '../components/Carousel';
+import Steps from '../components/Steps';
+import About from '../components/About';
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Carousel />
+      <Steps />
+      <About />
     </>
   );
 };
